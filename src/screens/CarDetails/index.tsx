f66525c5bc40f0638794b1a893096e9d3d439110
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-02-26 17:10:43 -0300

    style: slider car details

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import { BackButton } from '../../components/BackButton'
+import { ImageSlider } from '../../components/ImageSlider'
 
 import {
   Container,
   Header,
+  CarImages
 } from './styles'
 
 export function CarDetails() {
   return (
     <Container>
       <Header>
-        <BackButton onPress={() => {}} />
+        <BackButton onPress={() => { }} />
       </Header>
+      <CarImages>
+        <ImageSlider
+          imagesUrl={['https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png']}
+        />
+      </CarImages>
     </Container>
   )
 }
