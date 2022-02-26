8a1c4eae180e0dc9e19a8542ad95ce48dd368697
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-02-26 19:49:15 -0300

    feat: button component created and stylized

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BackButton } from '../../components/BackButton'
 import { ImageSlider } from '../../components/ImageSlider'
 import { Accessory } from '../../components/Accessory'
+import { Button } from '../../components/Button'
 
 import speedSvg from '../../assets/speed.svg'
 import accelerationSvg from '../../assets/acceleration.svg'
@@ -24,6 +25,7 @@ import {
   Price,
   About,
   Accessories,
+  Footer,
 } from './styles'
 
 export function CarDetails() {
@@ -65,7 +67,24 @@ export function CarDetails() {
           indultado na praça Real Maestranza de Sevilla. É um belíssimo carro
           para quem gosta de acelerar.
         </About>
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide
+          indultado na praça Real Maestranza de Sevilla. É um belíssimo carro
+          para quem gosta de acelerar.
+        </About>
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide
+          indultado na praça Real Maestranza de Sevilla. É um belíssimo carro
+          para quem gosta de acelerar.
+        </About>
       </Content>
+
+      <Footer>
+        <Button
+          title='Escolher período do aluguel'          
+        />
+      </Footer>
+
     </Container>
   )
 }
