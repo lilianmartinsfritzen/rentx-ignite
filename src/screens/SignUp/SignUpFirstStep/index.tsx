7e12210b43ba39c6c0236b4f50ff7f07b69baf7c
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-04-23 23:13:52 -0300

    feat: validated registration form

--- src/screens/SignUp/SignUpFirstStep/index.tsx
+++ src/screens/SignUp/SignUpFirstStep/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { 
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
-  Keyboard
+  Keyboard,
+  Alert
 } from 'react-native'
 import { CommonActions, useNavigation } from '@react-navigation/native'
+import * as Yup from 'yup'
+
 import { BackButton } from '../../../components/BackButton'
 import { Bullet } from '../../../components/Bullet'
 import { Button } from '../../../components/Button'
@@ -21,18 +24,44 @@ import {
 } from './styles'
 
 export function SignUpFirstStep() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [driverLicense, setDriverLicense] = useState('')
+
   const navigation = useNavigation()
 
   function handleBack() {
     navigation.goBack()
   }
 
-  function handleNextStep() {
-    navigation.dispatch(
-      CommonActions.navigate({
-        name: 'SignUpSecondStep'
+  async function handleNextStep() {
+    try {
+      const schema = Yup.object().shape({
+        driverLicense: Yup.string()
+          .required('CNH é obrigatória'),
+        email: Yup.string()
+          .email('E-mail é inválido')
+          .required('E-mail é obrigatório'),
+        name: Yup.string()
+          .required('Nome é obrigatório'),
       })
-    )
+
+      const data = { name, email, driverLicense }
+      await schema.validate(data)
+
+      navigation.dispatch(
+        CommonActions.navigate({
+          name: 'SignUpSecondStep',
+          params: {
+            user: data
+          }
+        })
+      )
+    } catch (error) {
+      if(error instanceof Yup.ValidationError) {
+        return Alert.alert('Opa', error.message)
+      }
+    }
   }
 
   return (
@@ -65,16 +94,22 @@ export function SignUpFirstStep() {
             <Input 
               iconName='user'
               placeholder='Nome'
+              onChangeText={setName}
+              value={name}
             />
             <Input 
               iconName='mail'
               placeholder='E-mail'
               keyboardType='email-address'
+              onChangeText={setEmail}
+              value={email}
             />
             <Input 
               iconName='credit-card'
               placeholder='CNH'
               keyboardType='numeric'
+              onChangeText={setDriverLicense}
+              value={driverLicense}
             />
           </Form>
 
