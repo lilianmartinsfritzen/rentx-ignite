3113437a14fc4964c354c0970c3112169e249c76
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-04-24 18:28:50 -0300

    feat: persisted data

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -2,22 +2,22 @@ import React, {
   createContext,
   useState,
   useContext,
-  ReactNode
+  ReactNode,
+  useEffect
 } from 'react'
 
 import api from '../services/api'
+import { database } from '../database'
+import { User as ModelUser } from '../database/model/User'
 
 interface User {
   id: string
+  user_id: string
   email: string
   name: string
   driver_license: string
   avatar: string
-}
-
-interface AuthState {
   token: string
-  user: User
 }
 
 interface SignInCredentials {
@@ -40,26 +40,58 @@ function AuthProvider({
   children 
 }: AuthProviderProps) {
   
-  const [data, setData] = useState<AuthState>({} as AuthState)
+  const [data, setData] = useState<User>({} as User)
 
   async function signIn({ email, password }: SignInCredentials) {
-    const response = await api.post('/sessions', {
-      email,
-      password
-    })
+    try {
+      const response = await api.post('/sessions', {
+        email,
+        password
+      })
+  
+      const { token, user } = response.data  
+      api.defaults.headers.common['Autorization'] = `Bearer ${token}`
+      
+      const userCollection = database.get<ModelUser>('users')
+      await database.write(async () => {
+        await userCollection.create(( newUser ) => {
+          newUser.user_id = user.id
+          newUser.name = user.name
+          newUser.email = user.email
+          newUser.driver_license = user.driver_license
+          newUser.avatar = user.avatar
+          newUser.token = token
+        })
+      })
 
-    const { token, user } = response.data
+      setData({ ...user, token })
 
-    api.defaults.headers.common['Autorization'] = `Bearer ${token}`
-
-    setData({ token, user })
+    } catch (error) {
+      console.log(error)
+    }
 
   }
+
+  useEffect(() => {
+    async function loadUserData() {
+      const userCollection = database.get<ModelUser>('users')
+      const response = await userCollection.query().fetch()
+
+      if(response.length > 0) {
+        const userData = response[0]._raw as unknown as User
+        api.defaults.headers.common['Autorization'] = `Bearer ${userData.token}`
+        setData(userData)
+      }
+    }
+
+    loadUserData()
+  }, [])
+  
 
   return (
     <AuthContext.Provider
       value={{
-        user: data.user,
+        user: data,
         signIn
       }}
     >
