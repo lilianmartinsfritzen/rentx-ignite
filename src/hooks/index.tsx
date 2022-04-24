235e6906a4d57281295d0c45bd55d1110084e70e
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-04-24 13:33:26 -0300

    feat: authentication context created

---
+++ src/hooks/index.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from 'react'
+
+import { AuthProvider } from './auth'
+
+interface AppProviderProps {
+  children: ReactNode
+}
+
+function AppProvider({ 
+  children 
+}: AppProviderProps) {
+  
+  return (
+    <AuthProvider>
+      {children}
+    </AuthProvider>
+  )
+}
+
+export { AppProvider }
