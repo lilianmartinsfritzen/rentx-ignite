f9b247c558547e75cb86354f422dcca90b9fe8b3
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-05-15 01:03:14 -0300

    fix: loop in routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,13 @@
-import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import { useAuth } from '../hooks/auth'
+import React from 'react';
+import { SplashRoutes } from './splash.routes'
 
-import { AppTabRoutes } from './app.tab.routes'
-import { AuthRoutes } from './auth.routes'
-import { LoadAnimation } from '../components/LoadAnimation'
 
 export function Routes() {
-  const { user, loading } = useAuth()
 
   return (
-    loading ? <LoadAnimation /> :
     <NavigationContainer>
-      { user.id ? <AppTabRoutes /> : <AuthRoutes /> }
+      <SplashRoutes />
     </NavigationContainer>
   )
 }
